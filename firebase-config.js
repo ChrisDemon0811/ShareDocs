71af5f3ac965cd2bb40ea06879ef915ad6f84f21
ChrisDemon0811 <2431540106@vaa.edu.vn>
2025-08-02 13:40:54 +0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBssVrhM7m6OKPX9nMLGsK5Ef5NBg-vHXc",
+    authDomain: "sharedoc-9036a.firebaseapp.com",
+    projectId: "sharedoc-9036a",
+    storageBucket: "sharedoc-9036a.firebasestorage.app",
+    messagingSenderId: "121763387103",
+    appId: "1:121763387103:web:6bf567a465321ce95c7b6a",
+    measurementId: "G-YDJYK7MKW3"
+};
